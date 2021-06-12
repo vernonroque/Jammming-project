e54f61c7ce0cd9e40e44817509bcad6d7ce52c8e
vroque <vroque88@gmail.com>
2021-06-12 15:00:20 -0500

    Add directories for searchbar_searchResults_and Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {SearchBar} from './SearchBar/SearchBar.js';
+import {SearchResults} from './SearchResults/SearchResults.js';
+import {Playlist} from './Playlist/Playlist.js';
 
 function App() {
   return (
@@ -18,8 +22,35 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+          <h1>Ja<span className="highlight">mmm</span>ing</h1>
+          <div className="App">
+            
+              <form className ="Search_Bar">
+                <input type = "text" placeholder="Enter a song title" name="search" />
+                <button type="submit">Submit</button> 
+                
+              </form>
+            <div className="App-playlist">
+  
+              <div className="Search_Results">
+
+              </div>
+             
+              <div className="Playlist">
+
+              </div>
+            </div>
+        </div>
+      </div>
     </div>
+
+
+
   );
 }
+
+
+
 
 export default App;
