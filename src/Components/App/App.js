f2ca452d8886cf8f648c1610c7df6f661be50bd8
vroque <vroque88@gmail.com>
2021-06-13 12:37:40 -0500

    Add Track and Tracklist directory, Remove default code from App_js file

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,56 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import {SearchBar} from './SearchBar/SearchBar.js';
 import {SearchResults} from './SearchResults/SearchResults.js';
 import {Playlist} from './Playlist/Playlist.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export class App extends React.Component{
+  render(){
+    return (
       <div>
-          <h1>Ja<span className="highlight">mmm</span>ing</h1>
-          <div className="App">
-            
-              <form className ="Search_Bar">
-                <input type = "text" placeholder="Enter a song title" name="search" />
-                <button type="submit">Submit</button> 
-                
-              </form>
-            <div className="App-playlist">
-  
-              <div className="Search_Results">
-
-              </div>
-             
-              <div className="Playlist">
-
-              </div>
-            </div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          {/*<!-- Add a SearchBar component -->*/}
+          <div className="App-playlist">
+            {/*<!-- Add a SearchResults component -->*/}
+            {/*<!-- Add a Playlist component -->*/}
+          </div>
         </div>
-      </div>
     </div>
-
-
-
-  );
+      );
+  }
 }
 
-
-
-
-export default App;
