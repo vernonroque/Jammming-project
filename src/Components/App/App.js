65c768ab360e578225c704a22b4bce8ae8135a3a
vroque <vroque88@gmail.com>
2021-06-13 20:08:35 -0500

    Pass down playlist to tracklist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,9 +7,11 @@ import {Playlist} from './Playlist/Playlist.js';
 export class App extends React.Component{
   constructor(props){
     super(props);
-    this.state = {searchResults:[]};
+    this.state = {searchResults:[],
+                  playlistName:'',
+                  playlistTracks:[{name:''},{artist:''},{album:''},{id:''}]
+                  };
   }
-  
   render(){
     return (
       <div>
@@ -21,7 +23,7 @@ export class App extends React.Component{
             {/*<!-- Add a SearchResults component -->*/}
               <SearchResults searchResults ={this.state.searchResults} />;
             {/*<!-- Add a Playlist component -->*/}
-              <Playlist />;
+              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />;
           </div>
         </div>
     </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -8,6 +8,7 @@ export class Playlist extends React.Component{
         <div className="Playlist">
             <input defaultValue={'New Playlist'}/>
             {/*<!-- Add a TrackList component -->*/}
+            <Tracklist tracks = {this.props.playlistTracks} />
             <button className="Playlist-save">SAVE TO SPOTIFY</button>
         </div>
         );
